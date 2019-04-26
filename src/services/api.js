be5df6708811a0806fb50107987a1b33860617ac
Matheus Barbosa <37550596+TheusBrz@users.noreply.github.com>
2019-04-26 11:03:24 -0300

    Update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://developer.datafarm.com.br/api',
+  baseURL: 'https://burguer-backend-brz.herokuapp.com/',
 });
 
 export default api;
